83bb24e73a46264787b9f73089ea0b6192b5d339
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-03-22 15:08:14 -0600

    Ejercicio 8

---
+++ leer un número y escriba su cuadrado/leer un número y escriba su cuadrado/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leer_un_número_y_escriba_su_cuadrado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero = 0;
+
+            Console.WriteLine("Ingresa un numero");
+            numero = double.Parse(Console.ReadLine());
+
+            double resultado = Math.Pow(numero, 2);
+            Console.WriteLine($"El numero {numero} levado al cuadrado es {resultado}");   
+
+            Console.ReadKey();
+        }
+    }
+}
