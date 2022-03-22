7bc2224cab84eded33902b967852c10f97e02ff4
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-03-22 14:52:50 -0600

    Promedio

---
+++ Promedio/Promedio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 0;
+            double B = 0;
+            double C = 0;
+           
+            Console.WriteLine("Ingresa el primer valor");
+            A = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa el segundo valor");
+            B = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa el tercer valor");
+            C = double.Parse(Console.ReadLine());
+
+            double promedio = (A + B + C) / 3;
+            Console.WriteLine($"El promedio del {A}  {B}  {C} es {promedio}");
+
+        }
+    }
+}
